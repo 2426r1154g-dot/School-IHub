81dee5415a4a11eb500d26a68b62fa1739bef710
2426r1154g-dot <2426r1154g@gse.okayama-c.ed.jp>
2026-09-10 20:41:57 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
-// Firebase Web App configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyAm5-GviQNDvZDlevCw5rHEFShIYmHfM-U",
+  apiKey: "AIzaSy...",
   authDomain: "school-i-hub.firebaseapp.com",
   projectId: "school-i-hub",
   storageBucket: "school-i-hub.firebasestorage.app",
