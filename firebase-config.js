6b8c2564e9ff48dae895241186b324cf6dc506f9
2426r1154g-dot <2426r1154g@gse.okayama-c.ed.jp>
2026-09-10 20:57:28 +0900

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "school-i-hub.firebaseapp.com",
-  projectId: "school-i-hub",
-  storageBucket: "school-i-hub.firebasestorage.app",
-  messagingSenderId: "1041415116313",
-  appId: "1:1041415116313:web:341a2d9d070d4f37587c88",
-  measurementId: "G-DMCE0YNEHD"
-};
